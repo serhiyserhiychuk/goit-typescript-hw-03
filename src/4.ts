164f5eea76dded3b44021d7be0fe48b2d110c9ff
serhiyserhiychuk <141840178+serhiyserhiychuk@users.noreply.github.com>
2024-04-05 17:28:04 -0400

    Doing Homework

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,49 @@
+class Key {
+  private signature: number = Math.random();
+  constructor(signature: number = 0) {
+    this.signature = signature !== 0 ? signature : Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean;
+  key: Key;
+  tenants: Person[];
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  openDoor(key: Key): void {}
+}
+
+class MyHouse extends House {
+  door: boolean = false;
+  constructor(key: Key) {
+    super();
+  }
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  openDoor(givenKey: Key): void {
+    if (givenKey.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +52,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
